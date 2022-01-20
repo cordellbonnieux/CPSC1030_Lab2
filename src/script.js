3bd93303df62c40335900e26fc6f1122ee84faea
Cordell <cordellbonnieux@gmail.com>
2022-01-19 21:37:25 -0800

    Completed the assignment! Now to launch via the school linux server.

--- src/script.js
+++ src/script.js
@@ -2,11 +2,11 @@ const bg = document.querySelector('body');
 let counter = 0, lastRecordedTime = 0;
 const delayTime = 4;
 const colors = {
-    0: 'pink',
-    1: 'blue',
-    2: 'red',
-    3: 'purple',
-    4: 'orange'
+    0: 'radial-gradient(circle, rgba(179,58,180,1) 0%, rgba(253,77,29,1) 49%, rgba(245,252,69,1) 93%);',
+    1: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);',
+    2: 'radial-gradient(circle, rgba(58,177,180,1) 0%, rgba(253,29,248,1) 49%, rgba(137,252,69,1) 93%);',
+    3: 'radial-gradient(circle, rgba(62,58,180,1) 0%, rgba(253,29,29,1) 49%, rgba(252,241,69,1) 93%);',
+    4: 'radial-gradient(circle, rgba(180,166,58,1) 9%, rgba(29,165,253,1) 49%, rgba(113,252,69,1) 93%);'
 }
 
 function step(timestamp) {
@@ -18,7 +18,7 @@ function step(timestamp) {
             counter = 0;
         }
         lastRecordedTime = interval;
-        bg.style.backgroundColor = colors[counter];
+        bg.setAttribute('style',`background: ${colors[counter]}`);
     }
     window.requestAnimationFrame(step);
 }
